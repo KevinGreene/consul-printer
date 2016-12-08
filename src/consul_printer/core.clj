6d4788997f5b54dad89c622d412032a4ecceb7e8
Kevin Greene <kevindgreene@gmail.com>
2016-12-07 19:46:58 -0500

    Updated components in core.clj

--- src/consul_printer/core.clj
+++ src/consul_printer/core.clj
@@ -1,6 +1,6 @@
 (ns consul-printer.core
   (:require [mount.core :as mount]
-            [consul-printer.printer :refer [timer]]))
+            [consul-printer.printer :refer [config timer consul-watcher]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
